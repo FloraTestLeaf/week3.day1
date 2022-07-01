75edc91e072350547601a4a811ddf5aefee67a21
FloraTestLeaf <107315151+FloraTestLeaf@users.noreply.github.com>
2022-07-01 23:52:55 +0530

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,16 @@
+package week3.day1.org.department;
+
+import week3.day1.org.college.College;
+
+public class Department extends College {
+
+	public void deptName() {
+		System.out.println("Department name is CIVIL");
+	}
+
+	
+	public void evana() {
+		// TODO Auto-generated method stub
+		System.out.println("dep evana");
+	}
+}
