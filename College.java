8823a8dcc956a45dd07a39fe63c9368eb4e679b0
FloraTestLeaf <107315151+FloraTestLeaf@users.noreply.github.com>
2022-07-01 23:52:27 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,37 @@
+package week3.day1.org.college;
+
+public class College {
+//---multi level la private na no access only public access
+//
+//    Package   :org.college
+//    Class     :College
+//    Methods   :collegeName(),collegeCode(),collegeRank()
+//
+//    Package   :org.department
+//    Class        :Department
+//    Methods   :deptName()
+//
+//    Package   :org.student
+//    Class        :Student
+//    Methods   :studentName(),studentDept(),studentId()
+// 
+//Description:
+//create above 3 class and call all your class methods into the Student using multilevel inheritance.
+	
+	
+	public void collegeName() {
+		System.out.println("college name is CSI");
+	}
+	
+	public void collegeCode() {
+		System.out.println("college code is 2433106");
+	}
+	
+	public void collegeRank() {
+		System.out.println("college name is 4");
+	}
+	
+public void evana() {
+	System.out.println("col evana");
+}
+}
