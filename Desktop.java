df79a8b31e1c8446634760310bc1fd3720eec585
FloraTestLeaf <107315151+FloraTestLeaf@users.noreply.github.com>
2022-07-01 23:50:37 +0530

    Add files via upload

---
+++ Desktop.java
@@ -0,0 +1,17 @@
+package week3.day1.assignment;
+
+public class Desktop extends Computer {
+
+	public void desktopSize() {
+		System.out.println("size is 54");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Desktop des=new Desktop();
+des.computerModel();
+des.desktopSize();
+
+	}
+
+}
